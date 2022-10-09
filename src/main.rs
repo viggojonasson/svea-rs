8b52b6557267c13c5721412fecdd96185e890416
Viggo Jonasson <41396129+viggojonasson@users.noreply.github.com>
2022-10-09 15:57:30 +0200

    update example

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,40 @@
 use webserver::{
     http::{QueryValue, Status},
     interceptor::Interceptor,
-    path::{queries::Queries, Path},
+    path::Path,
     request::Request,
     response::Response,
     server::Server,
 };
 
-async fn change_status(_request: Request, response: Response) -> Response {
-    let mut r = response;
+async fn append_query(_req: Request, res: Response) -> Response {
+    let mut r = res;
 
-    r.body.push_str(" this request was intercepted");
-
-    r.status = Status::ImATeapot;
-
-    r
-}
-
-async fn change_body(_request: Request, response: Response) -> Response {
-    let mut r = response;
-
-    r.body
-        .push_str(&format!("<h1>The new status code is {:#?}</h1>", r.status));
+    r.body.push_str(
+        "
+<br>
+<h1>Intercepted!</h1>
+        ",
+    );
 
     r
-}
-
-async fn activate_on(request: Request) -> bool {
-    if let Some(intercept) = request.path.queries.get_by_key("intercept".to_string()) {
-        if intercept == &QueryValue::F32(1.0) {
-            return true;
-        }
-    }
-
-    false
 }
 
 #[tokio::main]
 async fn main() {
-    let change_status_interceptor = Interceptor::builder()
-        .on_request(change_status)
-        .activate_on(activate_on)
-        .name("change_status");
-
-    let add_status_to_body_interceptor = Interceptor::builder()
-        .on_request(change_body)
-        .activate_on_all(true)
-        .name("add_status_to_body");
-
-    let path = Path::new("/test".to_string(), Queries::new());
-
     Server::new("localhost".to_string(), 3000)
-        .route(path, |req| {
-            let mut queries = String::from("<h1>You sent no queries!</h1>");
-
-            for (key, value) in req.path.queries.0 {
-                if queries == "<h1>You sent no queries!</h1>" {
-                    queries = String::from("");
-                }
-                queries.push_str(&format!("{}: {:#?}, ", key, value));
-            }
-
-            format!("<h1>Queries you sent</h1>{}", queries).into()
-        })
-        .interceptor(change_status_interceptor)
-        .interceptor(add_status_to_body_interceptor)
+        .route(
+            Path::builder()
+                .path("/test")
+                .query("query", QueryValue::Bool(Some(false))),
+            |_req| "<h1>This is a test path</h1>".to_string().into(),
+        )
+        .interceptor(
+            Interceptor::builder()
+                .name("append query")
+                .on_request(append_query)
+                .activate_on_all(true),
+        )
         .fallback(|_request| {
             Response::builder()
                 .status(Status::NotFound)
