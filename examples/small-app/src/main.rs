a9e2c7752eb5262dabe647f46987cd99f52171b5
Viggo Jonasson <41396129+viggojonasson@users.noreply.github.com>
2022-11-05 22:21:21 +0100

    docs: add more example code

--- examples/small-app/src/main.rs
+++ examples/small-app/src/main.rs
@@ -1,15 +1,3 @@
-# webserver-rs
-
-Just playing around with Rust and web servers. Trying to learn rust.
-
-### Features
-
-- Blazingly slow
-- Unoptimized
-
-### Example Code:
-
-```rs
 use webserver::{
     http::Status,
     router::{route::Route, Router},
@@ -31,11 +19,3 @@ async fn main() {
         .run()
         .await;
 }
-```
-
-## Run Examples:
-
-- `file-serving`
-- - `cargo run --bin file-serving`
-- `example-app`
-- - `cargo run --bin example-app`
