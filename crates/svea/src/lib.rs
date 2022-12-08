8918abd0cf8c1eccb564a1de21f5271196dd4408
Viggo Jonasson <41396129+viggojonasson@users.noreply.github.com>
2022-12-08 13:36:29 +0100

    feat: add prelude
    Smaller import code for applications using svea

--- crates/svea/src/lib.rs
+++ crates/svea/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod handler;
+pub mod prelude;
 pub mod router;
 pub mod server;
 pub mod service;
---
+++ crates/svea/src/prelude.rs
@@ -0,0 +1,6 @@
+pub use crate::filter::*;
+pub use crate::http::*;
+pub use crate::router::route::*;
+pub use crate::router::*;
+pub use crate::server::Server;
+pub use crate::service::*;
